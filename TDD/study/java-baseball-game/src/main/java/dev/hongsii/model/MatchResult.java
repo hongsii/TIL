0b4be2d5af78336362e349f782f654c202d31051
Sihong Park <hongsii.dev@gmail.com>
2019-02-24 18:46:14 +0900

    정답 기능 추가

--- TDD/study/java-baseball-game/src/main/java/dev/hongsii/model/MatchResult.java
+++ TDD/study/java-baseball-game/src/main/java/dev/hongsii/model/MatchResult.java
@@ -1,8 +1,6 @@
 package dev.hongsii.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class MatchResult {
 
@@ -22,6 +20,19 @@ public class MatchResult {
 
     public void record(boolean isCorrectDigit, boolean isCorrectPosition) {
         resultTypes.add(ResultType.of(isCorrectDigit, isCorrectPosition));
+    }
+
+    public boolean isAllStrike() {
+        return resultTypes.stream()
+                .allMatch(resultType -> resultType == ResultType.STRIKE);
+    }
+
+    public Map<ResultType, Integer> getCountByResult() {
+        Map<ResultType, Integer> results = new LinkedHashMap<>();
+        for (ResultType resultType : resultTypes) {
+            results.put(resultType, results.getOrDefault(resultType, 0) + 1);
+        }
+        return Collections.unmodifiableMap(results);
     }
 
     public List<ResultType> getResultTypes() {
--- TDD/study/java-baseball-game/src/test/java/dev/hongsii/model/MatchResultTest.java
+++ TDD/study/java-baseball-game/src/test/java/dev/hongsii/model/MatchResultTest.java
@@ -2,6 +2,7 @@ package dev.hongsii.model;
 
 import org.junit.Test;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatchResultTest {
@@ -13,5 +14,19 @@ public class MatchResultTest {
         matchResult.record(true, true);
 
         assertThat(matchResult.getResultTypes()).hasSize(1).containsOnly(ResultType.STRIKE);
+    }
+
+    @Test
+    public void isAllStrike() {
+        MatchResult matchResult = MatchResult.of(asList(ResultType.STRIKE, ResultType.STRIKE, ResultType.STRIKE));
+
+        assertThat(matchResult.isAllStrike()).isTrue();
+    }
+
+    @Test
+    public void isNotOfStrike() {
+        MatchResult matchResult = MatchResult.of(asList(ResultType.BALL, ResultType.STRIKE, ResultType.STRIKE));
+
+        assertThat(matchResult.isAllStrike()).isFalse();
     }
 }
